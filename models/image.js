fa6e22ea1af6a5f79c41c56c045d2e116ef1abc6
Benjamin Huang <coolconfucius@yahoo.com>
2016-02-05 17:43:31 -0800

    refactored code. Can now upload multiple images insetad of just one

--- models/image.js
+++ models/image.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var aws = require('aws-sdk');
+var async = require('async'); 
+var uuid = require('node-uuid');
+var s3 = new aws.S3();
+var multer = require('multer');
+var upload = multer({ storage: multer.memoryStorage() });
 var mongoose = require('mongoose');
 var User = require('../models/user');
 var Image = require('../models/image');
@@ -23,6 +29,40 @@ var imageSchema = new mongoose.Schema({
   //   date: Date 
   // }]
 }); 
+
+imageSchema.statics.upload = function(files, done) {
+  async.map(files, uploadFile, done); 
+};
+
+function uploadFile(file, cb) {
+  var filename = file.originalname.replace(/ /g,''); 
+  var data = file.buffer; 
+
+  var match = filename.match(/\.\w+$/);
+  var ext = match ? match[0] : '';
+  var key = uuid.v1() + ext; 
+
+  var params = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: key, 
+    Body: data
+  };
+
+  s3.putObject(params, function(err) {
+    if(err) return cb(err);
+    var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + key;
+    // var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + filename;
+    var image = new Image({
+      filename: filename, 
+      url: url,
+      key: key
+    });
+    image.save(function(err, savedImage) {
+      if (err) return cb(err);
+      cb(null, savedImage);
+    })
+  });
+}
 
 Image = mongoose.model('Image', imageSchema);
 
--- routes/images.js
+++ routes/images.js
@@ -16,6 +16,7 @@ var Album = require('../models/album');
 var Image = require('../models/image');
 var User = require('../models/user');
 router.use(authMiddleware);
+
 // router.post('/', upload.array('images'), function(req, res) {
 //   console.log('req.files:', req.files);
 //   // var filename = "maru.jpg";
@@ -52,49 +53,78 @@ router.use(authMiddleware);
 // });
 
 // post image to an album. 
+// router.post('/:album', upload.array('images'), function(req, res) {
+//   Album.findById(req.params.album).exec(function(err, album){
+
+//     console.log('req.files:', req.files);
+//     var filename = req.files[0].originalname;
+//     var buffer = req.files[0].buffer;
+
+//     var match = filename.match(/\.\w+$/);
+//     var ext = match ? match[0] : '';
+//     var key = uuid.v1() + ext; 
+
+//     var params = {
+//       Bucket: process.env.AWS_BUCKET,
+//       Key: filename,  // name of file on s3
+//       Body: buffer      // content of file
+//     };
+
+//     s3.putObject(params, function(err, data) {  //uploads to s3
+//       if (err) return console.log(err);
+//       // var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + key;
+//       var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + filename;
+
+//       var image = new Image({
+//         filename: filename, 
+//         url: url, 
+//         key: key
+//       });
+//       image.save(function(err, savedImage) {
+//         if (err) {res.status(400).send(err)};
+//         album.images.push(image._id);
+//         album.save(function(err, savedAlbum) {
+//           if(err) res.status(400).send(err);
+//           res.redirect('/albums/'+req.params.album+'/'+req.user._id);
+//           // res.status(err ? 400 : 200).send(err || savedAlbum); 
+//           // console.log('url:', url);
+//           // res.redirect('/');
+//           // // res.render('index', { title: 'Express', imageurl: url });
+//         });
+//       });
+//     });
+  
+//   });
+// });
+
+// Refactored: post image to an album. 
 router.post('/:album', upload.array('images'), function(req, res) {
-  Album.findById(req.params.album).exec(function(err, album){
-
-    console.log('req.files:', req.files);
-    var filename = req.files[0].originalname;
-    var buffer = req.files[0].buffer;
-
-    var match = filename.match(/\.\w+$/);
-    var ext = match ? match[0] : '';
-    var key = uuid.v1() + ext; 
-
-    var params = {
-      Bucket: process.env.AWS_BUCKET,
-      Key: filename,  // name of file on s3
-      Body: buffer      // content of file
+  User.findById(req.user._id, function(err, user) {
+    if (err || !user) { 
+      return res.status(err ? 400 : 404).send(err || 'user not found');
+    }
+    if (user.albums.indexOf(req.params.album) === -1 ) {
+      return res.status(401).send();
     };
 
-    s3.putObject(params, function(err, data) {  //uploads to s3
-      if (err) return console.log(err);
-      // var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + key;
-      var url = process.env.AWS_URL + process.env.AWS_BUCKET + '/' + filename;
+    Album.findById(req.params.album).exec(function(err, album){
+      if (err || !album) return res.status(err ? 400 : 404).send(err || 'album not found');
 
-      var image = new Image({
-        filename: filename, 
-        url: url, 
-        key: key
-      });
-      image.save(function(err, savedImage) {
-        if (err) {res.status(400).send(err)};
-        album.images.push(image._id);
-        album.save(function(err, savedAlbum) {
-          if(err) res.status(400).send(err);
+      Image.upload(req.files, function(err, images){
+        if (err) return res.status(400).send(err);
+        var imageIds = images.map(image => image._id);
+
+        album.images = album.images.concat(imageIds);
+        album.save(function(err){
+          // res.status(err ? 400 : 200).send(err || null);
           res.redirect('/albums/'+req.params.album+'/'+req.user._id);
-          // res.status(err ? 400 : 200).send(err || savedAlbum); 
-          // console.log('url:', url);
-          // res.redirect('/');
-          // // res.render('index', { title: 'Express', imageurl: url });
         });
       });
     });
-  
   });
 });
+
+
 
 
 // Show an image from an album
@@ -129,54 +159,6 @@ router.delete('/:album/:image', function(req, res, next){
     });
   });
 });
-
-
-
-// Add an image to favorites: 
-// Album gets points for images favorited. 
-// router.post('/fave/:album/:image', function(req, res, next){
-//   User.findById(req.user._id, function(err, user){
-//     Album.findById(req.params.album, function(err, album){
-//       if(err) res.status(400).send(err);
-//       Image.findById(req.params.image, function(err, image){
-//         if(err) res.status(400).send(err);
-
-//         album.points++; 
-//         album.save(function(err, savedAlbum){
-
-//           user.faveimages.push(image._id);
-//           user.save(function(err, savedUser){
-//             res.status(err ? 400 : 200).send(err || savedAlbum);
-//           });
-//         });
-//       });
-//     });    
-//   });
-// });
-
-
-// router.delete('/unfave/:image', function(req, res, next){
-//   User.findById(req.user._id, function(err, user){
-//     // Album.findById(req.params.album, function(err, album){
-//     //   if(err) res.status(400).send(err);
-//       Image.findById(req.params.image, function(err, image){
-//         if(err) res.status(400).send(err);
-
-//         // album.points++; 
-//         // album.save(function(err, savedAlbum){
-//           var index = user.faveimages.indexOf(image._id);
-//           if (index === -1) { console.log("Not found!"); return; };
-//           user.faveimages.splice(index, 1);
-          
-//           user.save(function(err, savedUser){
-//             res.status(err ? 400 : 200).send(err || savedUser);
-//           });
-//         });
-//     //   });
-//     // });    
-//   });
-// });
-
 
 
 module.exports = router;
